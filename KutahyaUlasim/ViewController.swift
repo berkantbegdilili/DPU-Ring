a07ad2d952ebcf492777af65eac50cc9c9bf9e96
Berkant BEGDILILI <59812010+berkantbegdilili@users.noreply.github.com>
2020-05-12 13:47:55 +0300

    Initial Commit

---
+++ KutahyaUlasim/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KutahyaUlasim
+//
+//  Created by Berkant Beğdilili on 12.05.2020.
+//  Copyright © 2020 Berkant Beğdilili. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
